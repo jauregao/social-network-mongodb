a855d05d118a6f38c7c7f9123c0233fab8f8fe5d
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-04-01 16:38:15 -0300

    feat: implementa a busca pelo id do usuario

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,40 @@
-import { Request, Response } from "express"
+import { Request, Response, query } from "express"
 import User from "../models/User"
 import { TUser } from "../types"
+import { Types } from "mongoose"
 
 export class UserController {
 
-  async create(req: Request, res: Response) {
+  async getAll(_: Request, res: Response){
+    try {
+      const users = await User.find()
+      return res.json(users)
+
+    } catch (error) {
+      return res.status(500).json({message: 'Internal Server Error'})
+    }
+  }
+
+  async getOne(req: Request, res: Response) {
+    const { id } = req.params
+    
+    if (!Types.ObjectId.isValid(id)) return res.status(400).json({ message: 'Invalid user ID.' })
+
+    try {
+      const user = await User.findById(id)
+      console.log("Usuário encontrado:", user);
+
+      if(user === null) return res.status(404).json({message: 'User not found.'})
+
+      return res.json(user)
+    } catch (error) {
+      console.error("Erro ao buscar usuário:", error)
+      return res.status(500).json({message: 'Internal Server Error'})
+      
+    }
+  }
+
+  async create(req: Request, res: Response): Promise<TUser | Object> {
     const userData = req.body as TUser
 
     try {
@@ -30,4 +60,5 @@ export class UserController {
       return res.status(500).json({message: 'Internal Server Error'})
     }
   }
+
 }
--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,16 @@ import { Router } from "express"
 import { UserController } from "./controllers/UserController"
 
 const routes = Router()
+const userController = new UserController();
 
 
 routes.post('/user',
-  new UserController().create)
+  userController.create)
 
+routes.get('/user/:id',
+  userController.getOne)
+  
+routes.get('/users',
+    userController.getAll)
 
 export default routes
